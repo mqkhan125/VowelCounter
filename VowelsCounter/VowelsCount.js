d0b8eae2c7012116725520dae9d1692019665d88
mqkhan125 <muhammadqasimkhan341@gmail.com>
2024-05-13 10:35:34 +0500

    vowels counter

---
+++ VowelsCounter/VowelsCount.js
@@ -0,0 +1,20 @@
+const textarea = document.querySelector('#textarea');
+const button = document.querySelector('#count');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', () => {
+    if(textarea.value === ""){
+        output.innerHTML = 'Please Enter your number';
+    }else {
+        //aeiou
+        const val = textarea.value;
+
+        let count = 0;
+        for (let i=0; i<val.length; i++){
+            if (val[i] === 'a' || val[i] === 'e' || val[i] === 'i' || val[i] === 'o' || val[i] === 'u'){
+                count++;
+            }
+        }
+        output.innerHTML = `The number of vowels are ${count}`;
+    }
+})
